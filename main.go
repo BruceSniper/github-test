d5ec094d71977ef76ae3fe84ad02bdbec71a50fa
zhangyuyao <zhangyuyao@shein.com>
2021-11-03 11:48:56 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+/**
+ * @Author:      zhangyuyao
+ * @Description: TODO
+ * @File:        main.go
+ * @Version:     1.0.0
+ * @Date:        2021/11/3 11:48 上午
+ */
+
+package main
+
+func main() {
+	println("hello world")
+}
